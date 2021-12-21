b85bb85452732ce53025fdd36c30925e355c8b51
LakkappaBJ <lucky.xworkz@gmail.com>
2021-12-21 11:32:10 +0530

    Hi

---
+++ A.java
@@ -0,0 +1,22 @@
+class A{
+  public static void main(String... args)
+  {
+System.out.println("Nice Bro....Well Done");
+  }
+}
+/*
+It can be write also main method
+public static void main(String[] args)
+* public static void main(String... args)
+* public static void main(String[]args)
+* public static void main(String args[])
+* public static void main(String []args)
+*static public void main(String[] args)
+    
+class A{
+  public static void main(String... args)
+  {
+	System.out.println("Nice Bro....Well Done");
+  }
+};     <- Having semicolon at the end of class...		 
+*/
